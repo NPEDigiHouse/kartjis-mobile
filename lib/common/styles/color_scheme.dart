401ebfc1889e3061e4b9b582ea36e49347e8e6d8
fajri-rasid1st <fajrimuhammad460@gmail.com>
2023-04-11 01:06:04 +0800

    add some common files

---
+++ lib/common/styles/color_scheme.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+// Colors
+const primaryColor = Color(0XFF2C1F63);
+const secondaryColor = Color(0XFFDF135C);
+const tertiaryColor = Color(0XFFFFC351);
+const backgroundColor = Color(0XFFF6F7F9);
+const scaffoldBackgroundColor = Color(0XFFFFFFFF);
+const primaryTextColor = Color(0XFF1E293B);
+const secondaryTextColor = Color(0XFFBBBEC1);
+const dividerColor = Color(0XFFE4E4E4);
+const errorColor = Color(0XFFD90429);
+
+// Color scheme
+final colorScheme = ColorScheme.fromSeed(
+  seedColor: primaryColor,
+  brightness: Brightness.light,
+  primary: primaryColor,
+  onPrimary: scaffoldBackgroundColor,
+  secondary: secondaryColor,
+  onSecondary: scaffoldBackgroundColor,
+  tertiary: tertiaryColor,
+  onTertiary: scaffoldBackgroundColor,
+  background: backgroundColor,
+  onBackground: primaryTextColor,
+  error: errorColor,
+  errorContainer: errorColor,
+  onError: scaffoldBackgroundColor,
+);
---
+++ lib/common/utils/routes.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+// Add some routes name here ...
+
+// Register the RouteObserver as a navigation observer
+final routeObserver = RouteObserver<ModalRoute<void>>();
